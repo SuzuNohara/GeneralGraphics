dd89fa5ec1b3aaf37502f8743541680031715630
SuzuNohara <suzu.nkdca@gmail.com>
2023-04-10 11:41:38 -0600

    chess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NodesComponent } from './nodes/nodes.component';
+import { ChessComponent } from './chess/chess.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: NodesComponent,
+  path: 'nodes'
+},{
+  component: ChessComponent,
+  path: 'chess'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NodesComponent } from './nodes/nodes.component';
+import { ChessComponent } from './chess/chess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodesComponent,
+    ChessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
